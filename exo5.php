4d2aa9e6c2116e098f22bb74ec7035708bdb2270
Alison-Denis <74008946+Alison-Denis@users.noreply.github.com>
2020-12-07 14:06:48 +0100

    Add files via upload

---
+++ exo5.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>PhpPart1Exo5</title>
+</head>
+
+<body>
+<?php
+$age = NULL;
+?>
+<p>Voici un age vide : <?php $age ?> .</p>
+</body>
+</html>
